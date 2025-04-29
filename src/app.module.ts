8d9852dc5c183a70ab7b297e6c5066d44c9d66be
Bobby Bonestell <rbonestell@gmail.com>
2025-04-29 12:09:25 -0600

    Added SecretsModule to base AppModule in core project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,24 @@
-import { Module } from '@nestjs/common'
+import { Module } from "@nestjs/common";
 
-import { AppController } from './app.controller'
-import { AppGateway } from './app.gateway'
-import { AppService } from './app.service'
-import { AuthModule } from './core/auth/auth.module'
-import { ConfigModule } from './core/config/config.module'
-import { LoggerModule } from './core/logger/logger.module'
-import { AccessoriesModule } from './modules/accessories/accessories.module'
-import { BackupModule } from './modules/backup/backup.module'
-import { ChildBridgesModule } from './modules/child-bridges/child-bridges.module'
-import { ConfigEditorModule } from './modules/config-editor/config-editor.module'
-import { CustomPluginsModule } from './modules/custom-plugins/custom-plugins.module'
-import { LogModule } from './modules/log/log.module'
-import { PlatformToolsModule } from './modules/platform-tools/platform-tools.module'
-import { PluginsModule } from './modules/plugins/plugins.module'
-import { ServerModule } from './modules/server/server.module'
-import { SetupWizardModule } from './modules/setup-wizard/setup-wizard.module'
-import { StatusModule } from './modules/status/status.module'
-import { UsersModule } from './modules/users/users.module'
+import { AppController } from "./app.controller";
+import { AppGateway } from "./app.gateway";
+import { AppService } from "./app.service";
+import { AuthModule } from "./core/auth/auth.module";
+import { ConfigModule } from "./core/config/config.module";
+import { LoggerModule } from "./core/logger/logger.module";
+import { SecretsModule } from "./core/secrets/secrets.module";
+import { AccessoriesModule } from "./modules/accessories/accessories.module";
+import { BackupModule } from "./modules/backup/backup.module";
+import { ChildBridgesModule } from "./modules/child-bridges/child-bridges.module";
+import { ConfigEditorModule } from "./modules/config-editor/config-editor.module";
+import { CustomPluginsModule } from "./modules/custom-plugins/custom-plugins.module";
+import { LogModule } from "./modules/log/log.module";
+import { PlatformToolsModule } from "./modules/platform-tools/platform-tools.module";
+import { PluginsModule } from "./modules/plugins/plugins.module";
+import { ServerModule } from "./modules/server/server.module";
+import { SetupWizardModule } from "./modules/setup-wizard/setup-wizard.module";
+import { StatusModule } from "./modules/status/status.module";
+import { UsersModule } from "./modules/users/users.module";
 
 @Module({
   imports: [
@@ -36,14 +37,9 @@ import { UsersModule } from './modules/users/users.module'
     BackupModule,
     LogModule,
     SetupWizardModule,
+    SecretsModule,
   ],
-  controllers: [
-    AppController,
-  ],
-  providers: [
-    AppService,
-    AppGateway,
-  ],
+  controllers: [AppController],
+  providers: [AppService, AppGateway],
 })
-
 export class AppModule {}
